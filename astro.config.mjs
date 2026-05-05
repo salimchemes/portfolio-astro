6d6326ef481b85505489428388bd0c92b1bea193
Salim Chemes <salimchemes@gmail.com>
2026-05-05 17:26:59 -0300

    Update site URL in Astro configuration to include 'www'

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://salimchemes.com',
+	site: 'https://www.salimchemes.com',
 	integrations: [mdx(), sitemap()],
 });
